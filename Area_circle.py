643c2cf1d778341778fbbf9cc51138bd63fb09a1
Belal ahmad <belalahmadswnbhr@gmail.com>
2022-12-13 21:21:40 +0530

    Calculate Area of Circle & Area of Rectangle

---
+++ Area_circle.py
@@ -0,0 +1,9 @@
+r=float(input("Enter the value of radius :"))
+area=3.14*r*r
+print("Area of cirlce",area)
+
+l=float(input("Enter the length of Rectangle:"))
+b=float(input("Enter the Breadth of Rectangle:"))
+Area=l*b
+print("Area of Rectangel:",Area)
+
