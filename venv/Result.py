92fd0fb7073b58ec6a53274c8ac3bb1a6ee54647
belaletech <belalahmadswnbhr@gmail.com>
2023-02-04 13:42:28 +0530

    To check the Result on the behalf of percetage

---
+++ venv/Result.py
@@ -0,0 +1,12 @@
+percentage=float(input("Enter your percentage\n"))
+if percentage>=100:
+    print("Invalid choice!!\n NOTE- Percentage should be less than or equal to 100")
+elif percentage>=60:
+
+        print("First Division")
+elif percentage>=45:
+    print("Second Division")
+elif percentage>=33:
+    print("Third Divison")
+else:
+    print("Sorry !! you fail")
